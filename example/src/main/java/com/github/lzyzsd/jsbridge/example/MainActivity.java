afac4e9b025f7569b86a87caf04bfef64b572f10
dengyuanting <dengyuanting@corp.netease.com>
2019-12-19 19:59:07 +0800

    Revert "最小api为17（google修复了js的安全性漏洞），改用默认android默认js接口对接方式"
    This reverts commit 91484031

--- example/src/main/java/com/github/lzyzsd/jsbridge/example/MainActivity.java
+++ example/src/main/java/com/github/lzyzsd/jsbridge/example/MainActivity.java
@@ -12,8 +12,10 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.widget.Button;
 
+import com.github.lzyzsd.jsbridge.BridgeHandler;
 import com.github.lzyzsd.jsbridge.BridgeWebView;
-import com.github.lzyzsd.jsbridge.OnBridgeCallback;
+import com.github.lzyzsd.jsbridge.CallBackFunction;
+import com.github.lzyzsd.jsbridge.DefaultHandler;
 import com.google.gson.Gson;
 
 public class MainActivity extends Activity implements OnClickListener {
@@ -51,6 +53,7 @@ public class MainActivity extends Activity implements OnClickListener {
 
 		button.setOnClickListener(this);
 
+		webView.setDefaultHandler(new DefaultHandler());
 
 		webView.setWebChromeClient(new WebChromeClient() {
 
@@ -77,9 +80,17 @@ public class MainActivity extends Activity implements OnClickListener {
 			}
 		});
 
-		webView.addJavascriptInterface(new MainJavascrotInterface(webView.getCallbacks(), webView), "android");
-		webView.setGson(new Gson());
 		webView.loadUrl("file:///android_asset/demo.html");
+
+		webView.registerHandler("submitFromWeb", new BridgeHandler() {
+
+			@Override
+			public void handler(String data, CallBackFunction function) {
+				Log.i(TAG, "handler = submitFromWeb, data from web = " + data);
+                function.onCallBack("submitFromWeb exe, response data 中文 from Java");
+			}
+
+		});
 
         User user = new User();
         Location location = new Location();
@@ -87,14 +98,14 @@ public class MainActivity extends Activity implements OnClickListener {
         user.location = location;
         user.name = "大头鬼";
 
-        webView.callHandler("functionInJs", new Gson().toJson(user), new OnBridgeCallback() {
+        webView.callHandler("functionInJs", new Gson().toJson(user), new CallBackFunction() {
             @Override
             public void onCallBack(String data) {
-				Log.d(TAG, "onCallBack: " + data);
+
             }
         });
 
-        webView.sendToWeb("hello");
+        webView.send("hello");
 
 	}
 
@@ -118,9 +129,7 @@ public class MainActivity extends Activity implements OnClickListener {
 
 			if(null == mUploadMessage && null != mUploadMessageArray){
 				Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
-				if (result != null) {
-					mUploadMessageArray.onReceiveValue(new Uri[]{result});
-				}
+				mUploadMessageArray.onReceiveValue(new Uri[]{result});
 				mUploadMessageArray = null;
 			}
 
@@ -130,7 +139,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	@Override
 	public void onClick(View v) {
 		if (button.equals(v)) {
-            webView.callHandler("functionInJs", "data from Java", new OnBridgeCallback() {
+            webView.callHandler("functionInJs", "data from Java", new CallBackFunction() {
 
 				@Override
 				public void onCallBack(String data) {
